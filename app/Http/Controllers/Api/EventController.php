c41dfeb871099810a6bd74758343995ebcd03447
NjauSamuel <njaus602@gmail.com>
2024-05-12 05:36:27 +0300

    Implemented authentication to some of the methods of the EventController class except for the index and show methods. Now the user needs to be authenticated in order to view the contents of those methods.

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,6 +14,10 @@ class EventController extends Controller
     use CanLoadRelationships;
     
     private  array $relations = ['user', 'attendees', 'attendees.user'];
+
+    public function __construct(){
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
     
     public function index()
     {
@@ -38,7 +42,7 @@ class EventController extends Controller
                 'end_time' => 'required|date|after:start_time',
             ]),
 
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
         return new EventResource($this->loadRelationships($event));
