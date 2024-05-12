0c7f0a6996969d9e37473feebe2e8786019995e7
NjauSamuel <njaus602@gmail.com>
2024-05-12 04:26:18 +0300

    Implemented a login functionality for the API by making an Auth controller having login and logout functionality and then registerd these two functionalities to the api.php route list.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,3 +13,5 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('events', EventController::class);
 
 Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except(['update']);
+
+Route::post('/login', [AuthController::class, 'login']);
