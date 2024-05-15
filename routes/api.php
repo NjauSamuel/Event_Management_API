c42aaf1d4f26630238a4bf41b8d5becb08ff68fe
NjauSamuel <njaus602@gmail.com>
2024-05-15 09:27:41 +0300

    Fixed a small bug that caused the index and show routes of the eventcontroller not to be shown.

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Events Routes that require authentication
 Route::apiResource('events', EventController::class)->middleware('auth:sanctum')->except(['index', 'show']);
+
+// Events Routes that don't require authentication
+Route::apiResource('events', EventController::class)->only(['index', 'show']);
 
 Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except(['update']);
 
